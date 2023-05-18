ae9276e17356e4d64b0c4dc4d66db0c6c5e5ac96
dbarjs <joseeduardobarros@gmail.com>
2023-05-17 21:40:09 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input v-model="spaceName" type="text">
+  </div>
+</template>
+
+<script lang="ts" setup>
+const {query} = useRoute();
+const router = useRouter();
+
+
+const spaceName = computed<string>({
+  get() {
+    return query['spaceName']
+  },
+  set(value: string) {
+    router.replace({
+      query: {
+        spaceName: value
+      }
+    })
+  }
+})
+
+</script>
