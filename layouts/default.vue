3b458e775faa237e8b494164451a7d7778e67340
dbarjs <joseeduardobarros@gmail.com>
2023-05-18 07:08:31 -0300

    Add integration with style hooks

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="$style.layout" :style="style" >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { appTheme } = useAppConfig();
+
+const { theme, themeOptions } = useAppTheme(appTheme);
+const { style } = useThemeStyle(theme, themeOptions);  
+</script>
+
+<style module lang="scss">
+.layout {
+  align-items: center;
+  background-color: var(--md-sys-color-background);
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="$style.field">
+    <input v-model="spaceName" type="text" placeholder="Hi!">
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { spaceName: spaceNameQuery } = useRouteThemeOptions();
+
+const spaceName = ref<string>(spaceNameQuery.value);
+
+watch(spaceName, (value) => {
+  spaceNameQuery.value = value;
+}, {
+  immediate: true
+});
+
+useHead({
+  title: spaceName
+})
+</script>
+
+<style module lang="scss">
+.field {
+  align-items: center;
+  color: var(--md-sys-color-on-background);
+  display: flex;
+  height: 100%;
+  width: 100%;
+
+  input {
+    -webkit-appearance: none;
+    outline: none;
+    color: inherit;
+    background: 0;
+    border: 0;
+    font-family: var(--system-ui);
+    font-size: 3rem;
+    text-align: center;
+    width: 100%;
+  }
+}
+</style>
